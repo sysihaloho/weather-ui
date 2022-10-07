015b237238b0fe5eb2addf23c61f6aed8c1d954a
Sandro Sihaloho <sysihaloho@gmail.com>
2022-10-07 12:28:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
-function App() {
+export default function BasicSelect() {
+  const [city, setCity] = React.useState('');
+
+  const [cities] = React.useState(
+    ['Melbourne', 'Sydney', 'Brisbane','Jakarta', 'Singapore', 'Tokyo', 'Bandung',
+  'Bangkok', 'New York', 'Paris', 'London', 'Madrid', 'Milan', 'Berlin']);
+
+  const [weather, setWeather] = React.useState(null);
+
+  const handleChange = (event) => {
+    setCity(event.target.value)
+    const url = "http://localhost:8080/getWeatherByCity/"+ event.target.value;
+    const fetchData = async () => {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            setWeather(json);
+        } catch (error) {
+            console.log("error", error);
+        }
+    };
+
+    fetchData();
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={{ minWidth: 120, padding:5}} >
+      <p>Select Location</p>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">City</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={city}
+          label="City"
+          onChange={handleChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {cities.map((row, index) => <MenuItem key={index} value={row}>{row}</MenuItem>)}
+        </Select>
+      </FormControl>
+
+      {weather !== null ? 
+        <Card sx={{ maxWidth: 275, marginTop: 5, backgroundColor: "lightblue" }} >
+        <CardContent>
+          <Typography sx={{ fontSize: 14, textAlign:"right" }} color="text.secondary" gutterBottom>
+            {weather.name}, {weather.sys.country}
+          </Typography>
+          <Typography sx={{ mb: 1.5, textAlign:"right" }} color="text.secondary">
+            {weather.weather[0].main}
+          </Typography>
+
+          <Typography variant="h3" component="div">
+            {Math.round(weather.main.temp)}°C
+          </Typography>
+        
+          <Typography variant="body2" color="text.secondary">
+            lat : {weather.coord.lat}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            lon : {weather.coord.lon}
+          </Typography>
+        </CardContent>
+      </Card> : ''}
+    </Box>
   );
 }
-
-export default App;
